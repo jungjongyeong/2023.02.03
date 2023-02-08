ebb4c22c790c8ff5b8b7ade16afacc0f57b5409e
정종영 <115339701+jungjongyeong@users.noreply.github.com>
2023-02-08 17:01:49 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -10,15 +10,36 @@ $('.slide-group').slick({
     pauseOnHover: false,
     fade: true,
 })
+
 $('.slide-group2').slick({
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true,
+    centerMode:true,
     prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>',
     speed:1000,
     slidesToShow: 3,
-    slidesToScroll:3,
+    slidesToScroll:1,
+    responsive: [{
+        breakpoint: 1025,
+        settings: {
+            slidesToShow: 1,
+            arrows:false,
+        }
+}]
+})
+
+// 재생, 멈춤버튼
+$('.article1 .plpa i').on('click', function(){ 
+    if($(this).hasClass('fa-pause')){
+        $('.article1 .slide-group').slick('slickPause');
+        $(this).removeClass('fa-pause').addClass('fa-play');
+        $('.article1 .slick-dots li.slick-active button').addClass('btnPause');
+    }else {
+        $('.article1 .slide-group').slick('slickPlay');
+        $(this).removeClass('fa-play').addClass('fa-pause');
+        $('.article1 .slick-dots li.slick-active button').removeClass('btnPause');
+    }
 })
 
 
